a39c147c61bbd143efaa5a0946cda1185d309660
harsh-sachan2244 <harsh.sachan224040@gmail.com>
2026-09-20 23:09:53 +0530

    Allow Vercel frontend in CORS

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ let port=process.env.PORT || 4000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://walletwave-lovat.vercel.app",
     credentials: true
 }))
 
