6febf0674810edcc7479159f1ca5f69fc023a785
harsh-sachan2244 <harsh.sachan224040@gmail.com>
2026-09-20 22:11:20 +0530

    Fix login cookie for cross origin authentication

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -97,7 +97,7 @@ console.log(error);
 res.cookie("token",token,{
 httpOnly:true,
 secure:process.env.NODE_ENVIRONMENT == "production",
-sameSite:"strict",
+sameSite:"none",
 maxAge:7*24*60*60*1000
 })
 
